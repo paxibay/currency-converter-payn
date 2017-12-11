ca4587754ce4f6957bbeaafefed10222142e4ff3
Payne <paynezh@yahoo.ca>
2017-12-11 08:28:42 -0500

    adding salesforce-ux/design-system and ng-lightning

--- src/app/converter/components/converter.component.ts
+++ src/app/converter/components/converter.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core'
 
 @Component({
   selector: 'converter',
-  template: 'converter.component.html',
+  templateUrl: 'converter.component.html',
   styleUrls: ['converter.component.scss']
 })
 export class ConverterComponent {
